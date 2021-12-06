1465555bf8f2f9915dee64908a59cdada4f60a8f
ahmad <aealaskar@gmail.com>
2021-12-06 20:24:54 +0300

    request-schema-edit

--- apis/request/request.routes.js
+++ apis/request/request.routes.js
@@ -1,31 +1,29 @@
 const express = require("express");
-const { requestListFetch, fetchRequest, requestCreate } = require("./request.controllers");
+const {
+  requestListFetch,
+  fetchRequest,
+  requestCreate,
+} = require("./request.controllers");
 const router = express.Router();
 const passport = require("passport");
 
 //Param Middleware
 router.param("requestId", async (req, res, next, requestId) => {
-    const request = await fetchRequest(requestId, next);
-    if (request) {
-      req.request = request;
-      next();
-    } else {
-      next({ status: 404, message: "Trip Not Found!" });
-    }
-  });
+  const request = await fetchRequest(requestId, next);
+  if (request) {
+    req.request = request;
+    next();
+  } else {
+    next({ status: 404, message: "Trip Not Found!" });
+  }
+});
 
-  // Routers
+// Routers
 router.get("/", requestListFetch);
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   requestCreate
 );
-
-
-
-
-
-
 
 module.exports = router;
